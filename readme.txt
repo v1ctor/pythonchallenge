f75921dbf26d14c7f9aa254ec12de2b6473b4916
Victor Buldakov <victor@buldakov.org>
2020-04-09 22:49:48 +0100

    roamnce

--- readme.txt
+++ readme.txt
@@ -11,10 +11,14 @@ Password: file
 4: http://www.pythonchallenge.com/pc/def/peak.html
 5: http://www.pythonchallenge.com/pc/def/channel.html
 6: http://www.pythonchallenge.com/pc/def/oxygen.html
-
+7: http://www.pythonchallenge.com/pc/def/integrity.html
+8: http://www.pythonchallenge.com/pc/return/good.html
+9: http://www.pythonchallenge.com/pc/return/bull.html
+10: http://www.pythonchallenge.com/pc/return/5808.html
 11: http://www.pythonchallenge.com/pc/return/evil.html
 12: http://www.pythonchallenge.com/pc/return/disproportional.html
 13: http://www.pythonchallenge.com/pc/return/italy.html
 14: http://www.pythonchallenge.com/pc/return/uzi.html
 15: http://www.pythonchallenge.com/pc/return/mozart.html
 16: http://www.pythonchallenge.com/pc/return/romance.html
+17: http://www.pythonchallenge.com/pc/stuff/violin.php
--- romance/run.py
+++ romance/run.py
@@ -1,8 +1,41 @@
 import requests
+import re
+import bz2
+from urllib.parse import unquote_to_bytes
+from xmlrpc.client import ServerProxy
 
 
 url = "http://www.pythonchallenge.com/pc/def/linkedlist.php"
+s = "next busynothing is (\d+)"
 
-r = requests.get(url, auth=("huge", "file"))
+def solve(n):
+    result = ""
+    while n is not None:
+        r = requests.get(url, params={"busynothing" : n})
+        result += r.cookies["info"]
+        if r.text is "Yes. Divide by two and keep going.":
+            n /= 2
+            continue
+        m = re.search(s, r.text)
+        if m is not None:
+            n = m.group(1)
+        else:
+            n = None
+    print(result)
 
-print(r.cookies["info"])
+
+
+
+first = 12345
+
+r = "BZh91AY%26SY%94%3A%E2I%00%00%21%19%80P%81%11%00%AFg%9E%A0+%00hE%3DM%B5%23%D0%D4%D1%E2%8D%06%A9%FA%26S%D4%D3%21%A1%EAi7h%9B%9A%2B%BF%60%22%C5WX%E1%ADL%80%E8V%3C%C6%A8%DBH%2632%18%A8x%01%08%21%8DS%0B%C8%AF%96KO%CA2%B0%F1%BD%1Du%A0%86%05%92s%B0%92%C4Bc%F1w%24S%85%09%09C%AE%24%90"
+
+print(bz2.decompress(unquote_to_bytes(r.replace("+", " "))))
+
+conn = ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
+
+print(conn.phone("Leopold"))
+
+
+# solve(first)
+
